f2aa2774f0e2f818990d5b6b53573583d47bb908
Shadmehr <56414145+shadmehr-salehi@users.noreply.github.com>
2023-02-09 23:59:15 +0330

    Finished

---
+++ Sorts/BucketSort.py
@@ -0,0 +1,19 @@
+#bucket sort implementation
+# ---- A Drunk Man Will Find His Way Home but a Drunk Bird May Get Lost Forever =) ----
+def bucketSort(array):
+    bucket = [[] for i in range(len(array))]
+    for j in array:
+        index_b = int(10 * j)
+        bucket[index_b].append(j)
+    for i in range(len(array)):
+        bucket[i] = sorted(bucket[i])
+    k = 0
+    for i in range(len(array)):
+        for j in range(len(bucket[i])):
+            array[k] = bucket[i][j]
+            k += 1
+    return array
+
+array = [.42, .32, .33, .52, .37, .47, .51]
+print("Sorted Array in descending order is")
+print(bucketSort(array))
