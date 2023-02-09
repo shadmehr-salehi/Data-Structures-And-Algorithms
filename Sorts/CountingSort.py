392b225252bec7abe432021d242a995748c63763
Shadmehr <56414145+shadmehr-salehi@users.noreply.github.com>
2023-02-10 00:00:08 +0330

    Finished

---
+++ Sorts/CountingSort.py
@@ -0,0 +1,25 @@
+#Countring Sort implementation
+# ---- A Drunk Man Will Find His Way Home but a Drunk Bird May Get Lost Forever =) ----
+def countingSort(array):
+    size = len(array)
+    output = [0] * size
+    count = [0] * 10
+    for i in range(0, size):
+        count[array[i]] += 1
+
+    for i in range(1, 10):
+        count[i] += count[i - 1]
+    i = size - 1
+    while i >= 0:
+        output[count[array[i]] - 1] = array[i]
+        count[array[i]] -= 1
+        i -= 1
+    array = output
+    
+    
+    
+    
+data = [4, 2, 2, 8, 3, 3, 1]
+countingSort(data)
+print("Sorted Array in Ascending Order: ")
+print(data)
