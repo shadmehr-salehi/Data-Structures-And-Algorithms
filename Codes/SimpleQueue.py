f24208f8fd20b2d10cf4df6b3f3de648d4a04d5f
Shadmehr <56414145+shadmehr-salehi@users.noreply.github.com>
2023-02-09 21:45:17 +0330

    Create SimpleQueue.py
    finished

---
+++ Codes/SimpleQueue.py
@@ -0,0 +1,15 @@
+#queue in python using list
+n = int(input())
+q = [0] * n 
+l = 0
+for i in range(n):
+    RawString = str(input())
+    if(len(RawString) > 4):
+        string = list(RawString.split(" "))
+        num = int(string[1])
+        q[l] = num
+        l = l+1
+    elif(len(RawString) == 3):
+        print(q[0])
+        q = q[1:n]
+        l = l-1    
